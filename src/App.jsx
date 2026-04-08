b3a606f4ac07d79b92a61ff94c281f0188ec796a
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-04-09 02:24:58 +0600

    prictice filter system

--- src/App.jsx
+++ src/App.jsx
@@ -1,104 +1,14 @@
 
 import './App.css'
-import ToDo from './todo';
-import Food from './food';
-import Singer from './singer';
-import Users from './users';
-import { Suspense } from 'react';
-import Comments from './Comments';
-// import Friends from './Friends'
-
-
-// const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
-//   .then(res => res.json());
-
-  // const friendsFatch = async()=> {
-  //   const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  //   return res.json();
-  // }
-
-
-  const commentFetch = async()=> {
-    const res = await fetch('https://jsonplaceholder.typicode.com/comments');
-    return res.json();
-  }
-
+import Filter from './NewTask/Filter'
+import Filter2 from './NewTask/Filter2'
 function App() {
-
-  const commentPromise = commentFetch();
-  // const frientsjPromise = friendsFatch();
-  console.log(commentPromise);
-
-
-  const singers = [
-    { id: 1, name: 'Dr. Mahfuz', age: 68 },
-    { id: 2, name: 'Tahsan', age: 45 },
-    { id: 3, name: 'Shuvro Dev', age: 57 }
-  ]
 
   return (
     <>
-      <h1>Vite + React</h1>
-
-      <Suspense fallback={<h2>Comments are comming...</h2>}>
-        <Comments commentPromise= {commentPromise}></Comments>
-      </Suspense>
-      {/* <Suspense  fallback={<h3>Loding...</h3>}>
-        <Users fetchUsers ={fetchUsers}></Users>
-      </Suspense> */}
-
-      {/* <Suspense fallback ={<h3>Friends are comming for treet</h3>}>
-        <Friends frientsjPromise={frientsjPromise}></Friends>
-      </Suspense> */}
-
-      {/* <ToDo task="Learn React"></ToDo> */}
-      {/* <Developer name="Mojumdar" tech="JAVA"></Developer>
-      <Developer name= "Rajon" tech="Python"></Developer>
-      <Developer name= "Sabana" tech="javaScript"></Developer>
-      <Student></Student>
-      <Person></Person> */}
-      {
-        singers.map(singer => <Singer key={singer.id} singer={singer}></Singer>)
-      }
-      <Food hungry="have to eat somthing" isHungry={true}></Food>
-      <Food hungry="im playing" isHungry={false}></Food>
-      <Food hungry="i want sleep" isHungry={true}></Food>
-
-
+    {/* <Filter></Filter> */}
+    <Filter2></Filter2>
     </>
-  )
-}
-
-function Developer({ name, tech }) {
-  // console.log(props);
-  return (
-    <div className='devloper'>
-      <h3>Developer: {name}</h3>
-      <p>Technology: {tech}</p>
-    </div>
-  )
-}
-
-function Person() {
-  return (
-    <div style={{
-      padding: '10px',
-      margin: '10px',
-      border: '2px solid red',
-      borderRadius: '20px'
-    }}>
-      <h3>Name:</h3>
-      <p>Dept:</p>
-    </div>
-  )
-}
-
-function Student() {
-  const styles = {
-    color: 'red',
-  }
-  return (
-    <h3 style={styles}>Name</h3>
   )
 }
 
